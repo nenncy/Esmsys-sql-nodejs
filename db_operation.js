e1f408d52bb268857ccb973229716347021cc713
nenncy <nencyvpatel3010@gmail.com>
2022-02-16 12:31:42 +0530

    update and delete query added !!

--- db_operation.js
+++ db_operation.js
@@ -3,6 +3,7 @@ const sql=require('mssql');
 const express = require('express');
 const app= express();
 const db_operation = require('./db_operation');
+const res = require('express/lib/response');
 const router = require('express').Router();
 
 
@@ -16,16 +17,133 @@ router.route('/getcountry').get( async (req,res)=>{
             .input('Country_name', req.query.name)
             .input('code', req.query.code)
             .execute(`insertCountry`);
-        const countrydata = result.recordset;
-        // console.log(res);
-        res.json(countrydata);
+           
+             
+
+
+
+            const countrydata = result.recordsets;
+            console.log(countrydata);
+        
+        res.status(200).json(countrydata);
 
     }
     //exception
     catch(err){
-      console.log(err);
+        res.status(500).json(error);
     }
     
+})
+
+
+//sp_country stored procedure call
+
+// select query ==>
+router.route('/spget').get( async (req,res)=>{
+  
+    var operation = req.body.operation;
+
+
+  try{
+    let pool= await sql.connect(config);
+    const result = await pool.request()
+    .input('Operation', req.query.op)
+     .execute('sp_country')
+     const countrydata=result.recordsets;
+  
+
+
+     //console.log(countrydata);
+    res.status(200).json(countrydata);
+  }
+  catch(err){
+       res.status(500).json(error);
+  }
+})
+
+
+//update query 
+router.route('/updatecountry').post( async(req,res)=>{
+    try{
+        let pool= await sql.connect(config);
+        const result = await pool.request()
+        .input('Operation', req.query.op)
+        .input('CountryName', req.query.countryname)
+        .input('update', req.query.update)
+        .execute('sp_country')
+        const countrydata = result;
+        if (result.rowsAffected<=0){
+            res.status(400).json({err: 'Its already in db'})
+        }
+        else{
+            res.status(200).json("row :"+ result.rowsAffected +"affected");
+        }
+        //console.log(res);
+        
+        res.status(200).json(countrydata);
+
+    }
+    catch(err){
+        res.status(500).json(error);
+    }
+})
+
+
+
+//insert query 
+router.route('/insertquery').post(async(req,res)=>{
+ 
+    try{
+        let pool= await sql.connect(config);
+        const result = await pool.request()
+        .input('Operation', req.query.op)
+        .input('CountryName', req.query.countryname)
+        .input('LangCountryName', req.query.lgname)
+        .input('CountryShortCode',req.query.csc)
+        .input('CreatedBy', req.query.cb)
+        
+        .execute('sp_country')
+
+        const detail= result;
+        if (result.rowsAffected<=0){
+            res.status(400).json({err: 'Its already in db'})
+        }
+        else{
+            res.status(200).json("row :"+ result.rowsAffected +"affected");
+        }
+
+
+        // res.status(200).json(detail);
+
+    }
+    catch(err){
+      res.status(500).json(err);
+    }
+
+});
+
+
+//delete query 
+router.route('/deletequery').post(async (req, res)=>{
+    try{
+        let pool= await sql.connect(config);
+        const result = await pool.request()
+        .input('Operation', req.query.op)
+        .input('Country_ID',req.query.id)
+        .execute('sp_country')
+
+        const update= result;
+        if (result.rowsAffected<=0){
+            res.status(400).json({err: 'There is some invalid details'})
+        }
+        else{
+            res.status(200).json(" number of row :"+ result.rowsAffected +" deleted");
+        }
+
+    }
+    catch(err){
+         res.status(500).json(err);
+    }
 })
 
 
